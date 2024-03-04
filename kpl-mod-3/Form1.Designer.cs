9517c3159f3e96988880aa66f2b5d5cd20022c31
Indra Mahesa <indramahesa128@gmail.com>
2024-03-04 07:54:04 +0700

    feat: fitur calculator

--- kpl-mod-3/Form1.Designer.cs
+++ kpl-mod-3/Form1.Designer.cs
@@ -28,12 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(293, 153);
+            button1.Name = "button1";
+            button1.RightToLeft = RightToLeft.Yes;
+            button1.Size = new Size(49, 46);
+            button1.TabIndex = 1;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(348, 153);
+            button2.Name = "button2";
+            button2.RightToLeft = RightToLeft.Yes;
+            button2.Size = new Size(49, 46);
+            button2.TabIndex = 2;
+            button2.Text = "=";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(403, 153);
+            button3.Name = "button3";
+            button3.RightToLeft = RightToLeft.Yes;
+            button3.Size = new Size(49, 46);
+            button3.TabIndex = 3;
+            button3.Text = "0";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(293, 205);
+            button4.Name = "button4";
+            button4.RightToLeft = RightToLeft.Yes;
+            button4.Size = new Size(49, 46);
+            button4.TabIndex = 4;
+            button4.Text = "1";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(348, 205);
+            button5.Name = "button5";
+            button5.RightToLeft = RightToLeft.Yes;
+            button5.Size = new Size(49, 46);
+            button5.TabIndex = 5;
+            button5.Text = "2";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(403, 205);
+            button6.Name = "button6";
+            button6.RightToLeft = RightToLeft.Yes;
+            button6.Size = new Size(49, 46);
+            button6.TabIndex = 6;
+            button6.Text = "3";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(293, 257);
+            button7.Name = "button7";
+            button7.RightToLeft = RightToLeft.Yes;
+            button7.Size = new Size(49, 46);
+            button7.TabIndex = 7;
+            button7.Text = "4";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.Location = new Point(348, 257);
+            button8.Name = "button8";
+            button8.RightToLeft = RightToLeft.Yes;
+            button8.Size = new Size(49, 46);
+            button8.TabIndex = 8;
+            button8.Text = "5";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(403, 257);
+            button9.Name = "button9";
+            button9.RightToLeft = RightToLeft.Yes;
+            button9.Size = new Size(49, 46);
+            button9.TabIndex = 9;
+            button9.Text = "6";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.Location = new Point(293, 309);
+            button10.Name = "button10";
+            button10.RightToLeft = RightToLeft.Yes;
+            button10.Size = new Size(49, 46);
+            button10.TabIndex = 10;
+            button10.Text = "7";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.Location = new Point(348, 309);
+            button11.Name = "button11";
+            button11.RightToLeft = RightToLeft.Yes;
+            button11.Size = new Size(49, 46);
+            button11.TabIndex = 11;
+            button11.Text = "8";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.Location = new Point(403, 309);
+            button12.Name = "button12";
+            button12.RightToLeft = RightToLeft.Yes;
+            button12.Size = new Size(49, 46);
+            button12.TabIndex = 12;
+            button12.Text = "9";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(256, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(141, 15);
+            label1.TabIndex = 13;
+            label1.Text = "Indra Mahesa 1302220067";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(256, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 14;
+            label2.Text = "Hasil :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(256, 66);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 15);
+            label3.TabIndex = 15;
+            label3.Text = "Operator : ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- kpl-mod-3/Form1.cs
+++ kpl-mod-3/Form1.cs
@@ -1,10 +1,192 @@
+using System.Numerics;
+
 namespace kpl_mod_3
 {
     public partial class Form1 : Form
     {
+        List<String> calculator = new List<String>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void updateUI()
+        {
+            String label = "Operator : ";
+            for (int i = 0; i < calculator.Count; i++)
+            {
+                label = label + " " + calculator[i];
+            }
+
+            label3.Text = label;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Tambah
+            if(calculator.Count == 0) 
+            {
+                return;
+            }
+            if (calculator[calculator.Count - 1] == "+")
+            {
+                return;
+            }
+            calculator.Add(button1.Text);
+            updateUI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Sama
+            int hasil = 0;
+            foreach (var cal in calculator)
+            {
+                int number;
+                if(int.TryParse(cal, out number))
+                {
+                    hasil += number;
+                }
+            }
+
+            label2.Text = "Hasil : " + hasil.ToString(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button3.Text);
+            updateUI();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            
+            calculator.Add(button4.Text);
+            updateUI();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button5.Text);
+            updateUI();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button6.Text);
+            updateUI();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button7.Text);
+            updateUI();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button8.Text);
+            updateUI();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button9.Text);
+            updateUI();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button10.Text);
+            updateUI();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button11.Text);
+            updateUI();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (calculator.Count > 0)
+            {
+                if (calculator[calculator.Count - 1] != "+")
+                {
+                    return;
+                }
+            }
+            calculator.Add(button12.Text);
+            updateUI();
         }
     }
 }
